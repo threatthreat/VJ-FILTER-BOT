134318f0bfe916a671f4aff56d11e80c34241a33
threatthreat <grezy8007@gmail.com>
2025-03-18 09:32:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movieaandseries7')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mww778')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movie_Film_series1')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mazzy70') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mazzy70')
 
